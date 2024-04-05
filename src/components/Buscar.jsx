f4bb503b381c40a0a9bf583a5e30dc4e5135fd09
Eduhassangordillo <hassangordillo.eduardo@gmail.com>
2024-04-04 21:48:23 -0300

    add muchas cosas

--- src/components/Buscar.jsx
+++ src/components/Buscar.jsx
@@ -1,9 +1,19 @@
 import style from "./../css/buscar.module.css";
 
+
+
 export const Buscar = () => {
   return (
     <div className={style.buscarCancha}>
+      <div className={style.container}>
+        <input type="text" className={style.imput} placeholder="Buscar cancha" />
+        <input type="text" className={style.imput} placeholder="Buscar cancha" />
+        <div className="">
+          <input type="text" placeholder="Buscar cancha" />
+          <input type="text" placeholder="Buscar cancha" />
+        </div>
 
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
     path: '/contact',
     element: <ContactPage />,
   },
+  
 
 ]);
 
