8524436bed0c3182b29a6cac9718bc825b3e8edf
Walo Tomas <gonzadh0109@gmail.com>
2024-04-07 16:29:31 -0300

    asdasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,53 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import Button from 'react-bootstrap/Button';
+import logo from '../img/ESTADIOVIRTUALNEGRO.png';
 
 export const NavBar = () => {
   return (
     <>
-        <Navbar expand="lg" className="bg-sucess">
-      <Container>
-        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+      <Navbar expand="lg" style={{backgroundColor: '#4CAF50'}}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="80"
+              height="40"
+              className="d-inline-block align-top"
+              alt="Estadio Virtual logo"
+            />
+          </Navbar.Brand>
+        </Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+        <Nav className="ml-auto" style={{ justifyContent: 'flex-end' }}>
+  <Button variant="link" href="#home">Home</Button>
+  <Button variant="link" href="#contact">Contacto</Button>
+  <Button 
+    variant="link" 
+    href="#login" 
+    style={{
+      boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)', 
+      transition: 'all 0.3s ease',
+      textDecoration: 'none'
+    }}
+    onMouseOver={(e) => {
+      e.currentTarget.style.backgroundColor = '#4CAF50';
+      e.currentTarget.style.color = 'white';
+    }}
+    onMouseOut={(e) => {
+      e.currentTarget.style.backgroundColor = '';
+      e.currentTarget.style.color = '';
+    }}
+  >
+    LOGIN
+  </Button>
+</Nav>
         </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      </Navbar>
     </>
   );
 };
+
 export default NavBar;
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.REACT_APP_GOOGLE_API_KEY': JSON.stringify(env.AIzaSyC85sVCSmoRAJGTMkbNer1t0TokfeM2T0o)
+    },
+    plugins: [react()],
+  }
 })
